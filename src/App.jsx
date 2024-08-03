71c80dca7961e4cf9980f141becfc5484f1f2035
Neteree <neteree@gmail.com>
2024-08-04 10:09:43 +1200

    Set up the idle animation for the player, the walk animation for when the player enters and leaves combat and finally the attack animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,47 @@
 import "./App.css";
-import { motion } from "framer-motion";
 import React, { useState } from "react";
 
 function App() {
-  const [isBoxMoved, setIsBoxMoved] = useState(false);
+  const [isInCombat, setIsBoxMoved] = useState(false);
 
-  const handleButtonClick = () => {
-    setIsBoxMoved(true);
+  const handleAnimationEnd = (event) => {
+    if (event.animationName == "walk") {
+      const player = event.target;
+      const weapon = player.querySelector(".weapon");
+
+      player.style.animation = "idle 0.5s steps(3) 1";
+      weapon.style.animation = "attack 0.5s steps(5) 1";
+      weapon.style.opacity = 1;
+    }
+
+    if (event.animationName == "idle") {
+      const player = event.target;
+      const weapon = player.querySelector(".weapon");
+      player.style.animation = "walkBack 0.75s steps(4) 1";
+      weapon.style.animation = "none";
+      weapon.style.opacity = 0;
+      player.style.translate = 0;
+    }
+
+    if (event.animationName == "walkBack") {
+      setIsBoxMoved(false);
+    }
   };
 
   return (
     <>
-      <button onClick={handleButtonClick}>{isBoxMoved ? "on" : "off"}</button>
-      <motion.div
+      <div
+        className="pixelated player"
+        onAnimationEnd={handleAnimationEnd}
         style={{
-          width: 100,
-          height: 100,
-          background: "red",
+          translate: isInCombat && "96px 0",
+          animation: isInCombat && "walk 0.75s steps(4) 1",
         }}
-        animate={{ y: isBoxMoved ? 320 : 0 }}
-        transition={{ duration: 2, repeat: 0 }}
-      />
+      >
+        <div className="pixelated weapon" style={{}} />
+      </div>
+
+      <button onClick={() => setIsBoxMoved(true)}>go</button>
     </>
   );
 }
