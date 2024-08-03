10d538d8fd3c6faaf620cd0df5dc2582b955ff1a
Neteree <neteree@gmail.com>
2024-08-03 16:58:39 +1200

    Set up some basic movement to prototype how it could work

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import "./App.css";
 import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 function App() {
+  const [isBoxMoved, setIsBoxMoved] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsBoxMoved(true);
+  };
+
   return (
-    <motion.div
-      style={{
-        width: 100,
-        height: 100,
-        background: "red",
-      }}
-      initial={{ x: 320 }}
-      animate={{ y: 250 }}
-      transition={{ duration: 2, repeat: 0 }}
-    />
+    <>
+      <button onClick={handleButtonClick}>{isBoxMoved ? "on" : "off"}</button>
+      <motion.div
+        style={{
+          width: 100,
+          height: 100,
+          background: "red",
+        }}
+        animate={{ y: isBoxMoved ? 320 : 0 }}
+        transition={{ duration: 2, repeat: 0 }}
+      />
+    </>
   );
 }
 
