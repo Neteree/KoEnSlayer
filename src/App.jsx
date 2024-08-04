6d84c628bfcdfc06bc9d0c8d51f3823740e3335a
Neteree <neteree@gmail.com>
2024-08-04 18:23:20 +1200

    You can now enter translations into a from to begin the battle between player and creature

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,37 @@ import Enemy from "./components/Enemy";
 
 function App() {
   const [isInCombat, setIsInCombat] = useState(false);
+  const [translation, setTranslation] = useState("");
+
+  const handleTranslation = (event) => {
+    setTranslation(event.target.value);
+  };
+
   return (
     <>
       <Player isInCombat={isInCombat} setIsInCombat={setIsInCombat} />
       <Enemy isInCombat={isInCombat} setIsInCombat={setIsInCombat} />
-      <button className="button" onClick={() => setIsInCombat(true)}>
-        go
-      </button>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setIsInCombat(true);
+          console.log(translation);
+        }}
+      >
+        <div className="translation-box">
+          <label className="translation-label" htmlFor="translation">
+            Translation:
+          </label>
+          <input
+            id="translation"
+            type="text"
+            value={translation}
+            onChange={handleTranslation}
+          />
+        </div>
+        <button type="submit">submit</button>
+      </form>
     </>
   );
 }
