801326b16227908caa0e460258f4e01b106f876b
Neteree <neteree@gmail.com>
2024-08-04 13:02:09 +1200

    Just did some minor changes with styling and so on, but nothing major

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,20 @@ function App() {
   const [isInCombat, setIsBoxMoved] = useState(false);
 
   const handleAnimationEnd = (event) => {
-    if (event.animationName == "walk") {
-      const player = event.target;
-      const weapon = player.querySelector(".weapon");
+    const player = event.target;
+    const weapon = player.querySelector(".weapon");
 
+    if (event.animationName == "walk") {
       player.style.animation = "idle 0.5s steps(3) 1";
       weapon.style.animation = "attack 0.5s steps(5) 1";
       weapon.style.opacity = 1;
     }
 
     if (event.animationName == "idle") {
-      const player = event.target;
-      const weapon = player.querySelector(".weapon");
       player.style.animation = "walkBack 0.75s steps(4) 1";
+      player.style.translate = 0;
       weapon.style.animation = "none";
       weapon.style.opacity = 0;
-      player.style.translate = 0;
     }
 
     if (event.animationName == "walkBack") {
@@ -40,8 +38,9 @@ function App() {
       >
         <div className="pixelated weapon" style={{}} />
       </div>
-
-      <button onClick={() => setIsBoxMoved(true)}>go</button>
+      <button className="button" onClick={() => setIsBoxMoved(true)}>
+        go
+      </button>
     </>
   );
 }
