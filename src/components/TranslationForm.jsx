a2234aab14e23852e6a85bf6e01441bfcbc2fa32
Neteree <neteree@gmail.com>
2024-08-05 15:10:35 +1200

    Turned the anonymous function that it is called when the translation form is submited into a named function instead and passed it in as an argument to OnSubmit instead of the original anonymous function

--- src/components/TranslationForm.jsx
+++ src/components/TranslationForm.jsx
@@ -9,13 +9,13 @@ function TranslationForm({
     setTranslationInput(event.target.value);
   };
 
+  const handleTranslationInputSubmission = (event) => {
+    event.preventDefault();
+    setIsInCombat(true);
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        setIsInCombat(true);
-      }}
-    >
+    <form onSubmit={handleTranslationInputSubmission}>
       <div className="translation-box">
         <label className="translation-label" htmlFor="translation">
           Translation Input:
